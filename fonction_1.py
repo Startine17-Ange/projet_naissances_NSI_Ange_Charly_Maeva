611822a666d6e1d38f5bde056e5aad2bdc3842d8
Startine17-Ange <startine17@gmail.com>
2025-01-24 16:09:23 +0100

    Delete fonction_1.py

--- fonction_1.py
+++
@@ -1,9 +0,0 @@
-## a entrer dans la console dico_fichier = open('PN_test.csv', 'r') :
-
-def extraire_fichier(dico_fichier):
-    li_dates = []
-    #li_sexes = []
-    for date, sexe in dico_fichier.items():
-        li_dates.append(date)
-        #li_sexes.append(sexe)
-    return li_dates
