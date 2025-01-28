495b4cbca4fec431847b0385a72e8aaa5895d8c3
maevajay <maevabachelier07@gmail.com>
2025-01-28 14:40:28 +0100

    Add files via upload

---
+++ premier_test_fonction_3.py
@@ -0,0 +1,19 @@
+﻿def nombre_date (liste_date):
+    liste_valeur=[0,0,0,0,0,0,0]
+    for dates in liste_date:
+        fonction_J(dates)
+        if fonction-J(dates)==lundi:
+            liste_valeur[1].append(1)
+        if fonction-J(dates)==mardi:
+            liste_valeur[2].append(1)
+        if fonction-J(dates)==mercredi:
+            liste_valeur[3].append(1)
+        if fonction-J(dates)==jeudi:
+            liste_valeur[4].append(1)
+        if fonction-J(dates)==vendredi:
+            liste_valeur[5].append(1)
+        if fonction-J(dates)==samedi:
+            liste_valeur[6].append(1)
+        if fonction-J(dates)==dimanche:
+            liste_valeur[7].append(1)
+    return liste_valeur
