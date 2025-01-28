bc6a44a28d44e4127ee60f2426e5a02dc4178b41
Startine17-Ange <startine17@gmail.com>
2025-01-28 15:51:24 +0100

    fonction_1 complete

--- fonction_1.py
+++ fonction_1.py
@@ -1,17 +1,19 @@
 ﻿## a entrer dans la console ch_fichier = open('PN_test.csv', 'r')
 
 def extraire_fichier(ch_fichier):
-    li_info = []
+    li_infos = []
     li_dates = []
     li_sexes = []
     for info in ch_fichier:
-        li_info.append(info)
-        li_info.split(";")
-    for i in range(len(li_info)):
-        if i % 2 == 1:
-            li_dates.append(li_info[i])
+        li_infos.append(info.split(";"))
+    for li_date_sexe in li_infos :
+        for i in range(len(li_date_sexe)):
+            if i % 2 == 0:
+                li_dates.append(li_date_sexe[i])
+##                li_dates.remove('Né(e) le')
         else :
-            li_sexes.append(li_info[i])
+            li_sexes.append(li_date_sexe[i])
+##            li_sexes.remove('Sexe\n')
     return li_dates
 
 ##et ne pas oublier ch_fichier.close()
