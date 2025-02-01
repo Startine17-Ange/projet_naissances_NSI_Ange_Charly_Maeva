a70132dfa26322efcc8747d65992de658083a690
maevajay <maevabachelier07@gmail.com>
2025-02-01 10:04:29 +0100

    Add files via upload

---
+++ fonction_3.py
@@ -0,0 +1,19 @@
+def nombre_date (liste_dates):
+    liste_valeur=[0,0,0,0,0,0,0]
+    for date in liste_dates:
+        fonction_J(date)
+        if fonction-J(date)=='lundi':
+            liste_valeur[0]=liste_valeur[0]+1
+        if fonction-J(date)=='mardi':
+            liste_valeur[1]=liste_valeur[1]+1
+        if fonction-J(date)=='mercredi':
+            liste_valeur[2]=liste_valeur[2]+1
+        if fonction-J(date)=='jeudi':
+            liste_valeur[3]=liste_valeur[3]+1
+        if fonction-J(date)=='vendredi':
+            liste_valeur[4]=liste_valeur[4]+1
+        if fonction-J(date)=='samedi':
+            liste_valeur[5]=liste_valeur[5]+1
+        if fonction-J(date)=='dimanche':
+            liste_valeur[6]=liste_valeur[6]+1
+    return liste_valeur
