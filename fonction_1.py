37415304d656c7afe1a7ca5869b491d20974fd3c
Startine17-Ange <startine17@gmail.com>
2025-02-07 18:24:14 +0100

    Update fonction_1.py
    pas sur que ca fonctionne je test dès que je peux, j'ai fait en sorte que ca renvoie un dico pour aider a la fonction 9

--- fonction_1.py
+++ fonction_1.py
@@ -4,16 +4,20 @@ def extraire_fichier(ch_fichier):
     li_infos = []
     li_dates = []
     li_sexes = []
+    dico_dates_sexes = {}
     for info in ch_fichier:
         li_infos.append(info.split(";"))
     for li_date_sexe in li_infos :
         for i in range(len(li_date_sexe)):
             if i % 2 == 0:
+                if dico_dates_sexes[date] in li_infos:
+                    dico_dates_sexes[date] += sexe
                 li_dates.append(li_date_sexe[i])
 ##                li_dates.remove('Né(e) le')
         else :
             li_sexes.append(li_date_sexe[i])
+            dico_dates_sexes[date] += sexe
 ##            li_sexes.remove('Sexe\n')
-    return li_dates, li_sexes
+    return li_dates, li_sexes, dico_dates_sexes
 
 ##et ne pas oublier ch_fichier.close()
