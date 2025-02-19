d8550ecb12125136d3f0355bf8dd551c8702da98
Startine17-Ange <startine17@gmail.com>
2025-02-19 16:31:08 +0100

    Update fonction_turtle.py

--- fonction_turtle.py
+++ fonction_turtle.py
@@ -1,12 +1,15 @@
-﻿from turtle import *
+from turtle import *
 
-##a mettre en full screen
 ##exemple du résultat de la fonction 6-9
-li_naissances_cycle_lune = [7,6,5,5,5,8,7,8,9,6,3,3,5,6,6,8,8,7,7,9,8,8,9,6,4,5,5,4,2, '.']
-li_naissances_cycle_lune_fille = [8,6,5,5,5,8,7,8,9,6,3,3,5,6,6,8,8,7,7,9,8,8,9,6,4,5,5,4,2, '.']
-li_naissances_cycle_lune_garcon = [9,6,5,5,5,8,7,8,9,6,3,3,5,6,6,8,8,7,7,9,8,8,9,6,4,5,5,4,2, '.']
-
+##li_naissances_cycle_lune = [7,6,5,5,5,8,7,8,9,6,3,3,5,6,6,8,8,7,7,9,8,8,9,6,4,5,5,4,2, '.']
+##li_naissances_cycle_lune_fille = [8,6,5,5,5,8,7,8,9,6,3,3,5,6,6,8,8,7,7,9,8,8,9,6,4,5,5,4,2, '.']
+##li_naissances_cycle_lune_garcon = [9,6,5,5,5,8,7,8,9,6,3,3,5,6,6,8,8,7,7,9,8,8,9,6,4,5,5,4,2, '.']
 ##à la place il faudra implanter les fonction, genre : fonction_6(li_dates)
+
+
+
+def turtle(li_naissances_cycle_lune, li_naissances_cycle_lune_fille, li_naissances_cycle_lune_garcon):
+##a mettre en full screen
 
 speed(30)
 penup()
